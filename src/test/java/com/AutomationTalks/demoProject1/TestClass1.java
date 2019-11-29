71878f581a566c799b99019eef0b9f402c283bba
Frank <fab30@hotmail.com>
2019-11-29 17:11:07 +0000

    setup grid

--- src/test/java/com/AutomationTalks/demoProject1/TestClass1.java
+++ src/test/java/com/AutomationTalks/demoProject1/TestClass1.java
@@ -1,24 +1,43 @@
 package com.AutomationTalks.demoProject1;
 
+import java.net.MalformedURLException;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class TestClass1 {
 	
-	
 	public static WebDriver driver;
+	
+	public static final String USERNAME = "frankboahene2";
+	  public static final String AUTOMATE_KEY = "g1Sk3KuAZsvPA7f7EcjY";
+	  public static final String URL = "https://" + USERNAME + ":" + AUTOMATE_KEY + "@hub-cloud.browserstack.com/wd/hub";
 	
 	
 	@BeforeMethod
-	public void launchDriver() {
+	public void launchDriver() throws MalformedURLException {
+		 DesiredCapabilities caps = new DesiredCapabilities();
+		 caps.setCapability("browser", "Chrome");
+		 caps.setCapability("browser_version", "70.0");
+		 caps.setCapability("os", "Windows");
+		 caps.setCapability("os_version", "7");
+		 caps.setCapability("resolution", "1024x768");
+		 caps.setCapability("project", "AutomationTalksProject");
+		 caps.setCapability("build", "Build1");
+		 caps.setCapability("name", "TestCaseName");
+		
+		 driver = new RemoteWebDriver(new java.net.URL(URL), caps);
+		
+		
 		//driver = new FirefoxDriver();
-		driver = new ChromeDriver();
+		//driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 	}
